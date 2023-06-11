8bc37b2e0e2bbf6cf8386c4cc4a9a129687784d5
Elena <lenkabeloglazova@yandex.ru>
2023-06-11 14:09:16 +0300

    HW1_1

--- HW/HW1_1.java
+++ HW/HW1_1.java
@@ -3,31 +3,30 @@ package HW;
 import java.util.Scanner;
 
 public class HW1_1 {
-
-    public static boolean isPrime(int n) {
-        if (n <= 1) {
-            return false;
+    public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Введите количество чисел в последовательности: ");
+            int n = scanner.nextInt();
+            int sum = 0;
+            System.out.println("Введите последовательность чисел:");
+            for (int i = 0; i < n; i++) {
+                int number = scanner.nextInt();
+                if (isPrime(number)) {
+                    sum += number;
+                }
+            }
+            System.out.println("Сумма простых чисел: " + sum);
         }
-        for (int i = 2; i <= Math.sqrt(n); i++) {
-            if (n % i == 0) {
+    public static boolean isPrime(int n) {
+            if (n <= 1) {
                 return false;
             }
-        }
-        return true;
-    }
-
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Введите количество чисел в последовательности: ");
-        int n = scanner.nextInt();
-        int sum = 0;
-        System.out.println("Введите последовательность чисел:");
-        for (int i = 0; i < n; i++) {
-            int number = scanner.nextInt();
-            if (isPrime(number)) {
-                sum += number;
+            for (int i = 2; i <= Math.sqrt(n); i++) {
+                if (n % i == 0) {
+                    return false;
+                }
             }
-        }
-        System.out.println("Сумма простых чисел: " + sum);
+            return true;
+        }   
     }
-}
+    
