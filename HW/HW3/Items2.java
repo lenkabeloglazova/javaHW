e241ffa928a758a493a5435609f138fddb5b7aed
Elena <lenkabeloglazova@yandex.ru>
2023-06-14 14:47:55 +0300

    HW3_1

--- HW/HW3/Items2.java
+++ HW/HW3/Items2.java
@@ -1,5 +1,13 @@
 package HW.HW3;
 
+@Data
+@AllArgsConstructor
 public class Items2 {
 
+
+    private String name;
+    private String countryName;
+    private Double price;
+    private Double weight;
+    private Integer sort;
 }
